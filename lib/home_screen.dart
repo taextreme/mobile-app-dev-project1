942482635cd9fd83d8077c9268afab2c33edd9a5
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-29 21:49:17 +0700

    Implement home screen of the app

---
+++ lib/home_screen.dart
@@ -0,0 +1,134 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import 'base_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EMERGENCY NUMBERS'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <BaseCard>[
+          BaseCard(
+            theColor: Colors.brown,
+            theChild: const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Police',
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            theOnTapFunc: () {
+              log("Call the Police!!!");
+            },
+          ),
+          BaseCard(
+            theColor: Colors.red,
+            theChild: const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Fire',
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            theOnTapFunc: () {
+              log("Fire Fire!!!!");
+            },
+          ),
+          BaseCard(
+              theColor: Colors.orange,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Traffic Officers',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Traffic!!!!");
+              }),
+          BaseCard(
+              theColor: Colors.grey,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Medical Emergency',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Ambulance!!!!");
+              }),
+          BaseCard(
+              theColor: Colors.lightGreen,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Disaster Control',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Disaster!!!!");
+              }),
+          BaseCard(
+              theColor: Colors.yellow,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Public Utilities Emergency',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Public Utilities!!!!");
+              }),
+          BaseCard(
+              theColor: Colors.blue,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Coastguard',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Coastguard!!!!");
+              }),
+          BaseCard(
+              theColor: Colors.teal,
+              theChild: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Emergency Radio Stations',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              theOnTapFunc: () {
+                log("Radio!!!!");
+              })
+        ],
+      ),
+    );
+  }
+}
