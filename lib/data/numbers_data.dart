879e0e12a5476a5edecc9c68101e1fa422a27d50
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-30 21:09:35 +0700

    Add data file and police numbers data

---
+++ lib/data/numbers_data.dart
@@ -0,0 +1,7 @@
+final policeNumbers = {
+  "General Police": "191",
+  "Tourist Police": "1155",
+  "Car Theft Suppression Police": "1192",
+  "Highway Patrol": "1193",
+  "Crime Suppression Police": "1195"
+};
