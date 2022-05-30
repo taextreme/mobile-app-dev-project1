469796b77d6adb874d64d22f9eade428dec0df5f
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-30 19:35:56 +0700

    Add data screen file

---
+++ lib/screen/data_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DataScreen extends StatelessWidget {
+  const DataScreen({required this.name, required this.dataMap});
+
+  final String name;
+  final Map<String, String> dataMap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
