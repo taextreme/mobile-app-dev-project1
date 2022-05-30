1567a9dcbb7515b47d188de2c3a7738c1612b382
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-30 21:09:16 +0700

    Update Data Screen

--- lib/screen/data_screen.dart
+++ lib/screen/data_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app_dev_project1/component/base_card.dart';
 
 class DataScreen extends StatelessWidget {
-  const DataScreen({required this.name, required this.dataMap});
+  const DataScreen(
+      {required this.name, required this.dataMap, required this.cardColor});
 
   final String name;
   final Map<String, String> dataMap;
+  final Color cardColor;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: dataMap.entries.map((entry) {
+          var w = BaseCard(
+              theColor: cardColor,
+              theChild: Column(
+                  children: <Widget>[Text(entry.key), Text(entry.value)]));
+          return w;
+        }).toList(),
+      ),
+    );
   }
 }
