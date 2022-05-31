20e1159625f0848a1a58eff6cc03ec280b4de75d
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-31 13:16:13 +0700

    Update & add more numbers data

--- lib/data/numbers_data.dart
+++ lib/data/numbers_data.dart
@@ -1,7 +1,66 @@
-const policeNumbers = {
+const Map<String, String> policeNumbers = {
   "General Police": "191",
-  "Tourist Police": "1155",
+  "Tourism Police": "1155",
   "Car Theft Suppression Police": "1192",
   "Highway Patrol": "1193",
   "Crime Suppression Police": "1195"
+};
+
+const Map<String, String> pandemicNumbers = {
+  "Medical Department": "1668",
+  "National Health Security Office": "1330",
+  "Covid-19 Pandemic Information Center": "1111",
+  "Disease Control Department": "1422",
+  "Social Security Office": "1506",
+  "Food and Drug Administration": "1556"
+};
+
+const Map<String, String> disasterNumbers = {
+  "Fire Department": "199",
+  "Disaster Warning Department": "192",
+  "Disaster Control Department": "1784",
+  "Pollution Control Department": "1650",
+  "Chemical Disaster Control and Support": "02-298-2387, 086-130-2386",
+  "Report Missing People": "1300"
+};
+
+const Map<String, String> medicalNumbers = {
+  "Medical Emergency Hotline": "1669",
+  "Bangkok Medical Service Hotline": "1646",
+  "Bangkok Paramedics Hotline": "1554",
+  "Police Hospital": "1691",
+  "Anti-Drugs Hotline": "1165",
+  "Toxicology Center": "1367",
+  "Thai Red Cross Society": "1664, 0-2256-4032-33",
+  "Phayathai Hospital Call Center": "1772",
+  "Mental Health Hotline": "1323"
+};
+
+const Map<String, String> travelNumbers = {
+  "Highways Department": "1586",
+  "Department of Land Transport": "1584",
+  "Traveling News Agency": "1672",
+  "Bangkok Mass Transit Authority": "1348",
+  "Expressway Authority of Thailand": "1543",
+  "Tourist Assistance Center": "021344077",
+  "Department of Rural Roads": "1146"
+};
+
+const Map<String, String> publicUtilitiesNumbers = {
+  "Metropolitan Electricity Authority": "1130",
+  "Provincial Electricity Authority": "1129",
+  "Metropolitan Waterworks Authority": "1125",
+  "Provincial Waterworks Authority": "1662",
+  "Royal Irrigation Department": "1460"
+};
+
+const Map<String, String> coastalNumbers = {
+  "Marine Safety Department Hotline": "1199",
+  "Coast Guard & Maritime emergency Hotline": "1196"
+};
+
+const Map<String, String> radioNumbers = {
+  "JS100 traffic Radio": "*1808, 02-350-3410-11(Emergency line)",
+  "FM91 Police Department Radio": "1644",
+  "Public Volunteers Radio": "1677"
 };
