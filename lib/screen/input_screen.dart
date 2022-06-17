e1020951bd7084dab6f3ee2f2111ff35725fc9fd
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-17 15:48:34 +0700

    Implement Personal Data Screen and Input screen

---
+++ lib/screen/input_screen.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import '../data/user_data.dart';
+
+class DataEntryScreen extends StatefulWidget {
+  const DataEntryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DataEntryScreen> createState() => _DataEntryScreenState();
+}
+
+class _DataEntryScreenState extends State<DataEntryScreen> {
+  late Map<String, dynamic> userData;
+  final _formKey = GlobalKey<FormState>();
+  String name = "";
+  String number = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initUserData();
+  }
+
+  void initUserData() {
+    String encodeData = UserSimplePreferences.getUserdata();
+    userData = json.decode(encodeData);
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Numbers Name',
+                filled: true,
+                isDense: true,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                setState(() {
+                  name = value!;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Numbers',
+                  filled: true,
+                  isDense: true,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  setState(() {
+                    number = value!;
+                  });
+                }),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                shadowColor: Colors.blueAccent,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0)),
+              ),
+              onPressed: () {
+                bool isValid = _formKey.currentState!.validate();
+                if (isValid) {
+                  _formKey.currentState!.save();
+                }
+                addNewData(name, number);
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void addNewData(String name, String number) {
+    userData[name] = number;
+    String encodeData = jsonEncode(userData);
+    UserSimplePreferences.setUserData(encodeData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Personal Emergency Number'),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(16),
+        child: _buildForm(),
+      ),
+    );
+  }
+}
---
+++ lib/screen/personal_data_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:mobile_app_dev_project1/component/base_card.dart';
+import 'package:mobile_app_dev_project1/data/user_data.dart';
+import 'package:mobile_app_dev_project1/screen/input_screen.dart';
+
+class PersonalDataScreen extends StatefulWidget {
+  const PersonalDataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PersonalDataScreen> createState() => _PersonalDataScreenState();
+}
+
+class _PersonalDataScreenState extends State<PersonalDataScreen> {
+  late Map<String, dynamic> userData;
+
+  @override
+  void initState() {
+    super.initState();
+    initUserData();
+  }
+
+  void initUserData() {
+    String encodeData = UserSimplePreferences.getUserdata();
+    userData = json.decode(encodeData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PERSONAL EMERGENCY NUMBERS'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: userData.entries.map((entry) {
+          var w = BaseCard(
+              theColor: Colors.green,
+              theChild: Column(children: <Widget>[
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Text(entry.key,
+                    style: const TextStyle(
+                        fontSize: 26, fontWeight: FontWeight.bold)),
+                Text(entry.value, style: const TextStyle(fontSize: 26))
+              ]));
+          return w;
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DataEntryScreen();
+          }));
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
