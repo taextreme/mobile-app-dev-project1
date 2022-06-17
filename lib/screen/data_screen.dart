79c7debba63c1bce354a01a8d10b1547e3c4e2b2
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-17 11:21:43 +0700

    Implement Basic Search Bar

--- lib/screen/data_screen.dart
+++ lib/screen/data_screen.dart
@@ -1,25 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:mobile_app_dev_project1/component/base_card.dart';
 
-class DataScreen extends StatelessWidget {
+class DataScreen extends StatefulWidget {
   const DataScreen(
-      {required this.name, required this.dataMap, required this.cardColor});
+      {Key? key,
+      required this.name,
+      required this.dataMap,
+      required this.cardColor})
+      : super(key: key);
 
   final String name;
   final Map<String, String> dataMap;
   final Color cardColor;
 
   @override
+  State<StatefulWidget> createState() => _DataScreenState();
+}
+
+class _DataScreenState extends State<DataScreen> {
+  late SearchBar searchBar;
+  late Map<String, String> data;
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+        title: Text(widget.name),
+        actions: [searchBar.getSearchAction(context)]);
+  }
+
+  void onSubmitted(String value) {
+    setState(() {
+      data = searchMap(value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    searchBar = SearchBar(
+        inBar: false,
+        setState: setState,
+        onSubmitted: onSubmitted,
+        buildDefaultAppBar: buildAppBar);
+    data = widget.dataMap;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(name),
-      ),
+      appBar: searchBar.build(context),
       body: ListView(
         scrollDirection: Axis.vertical,
-        children: dataMap.entries.map((entry) {
+        children: data.entries.map((entry) {
           var w = BaseCard(
-              theColor: cardColor,
+              theColor: widget.cardColor,
               theChild: Column(children: <Widget>[
                 const SizedBox(
                   height: 30.0,
@@ -33,5 +67,18 @@ class DataScreen extends StatelessWidget {
         }).toList(),
       ),
     );
+  }
+
+  Map<String, String> searchMap(String input) {
+    if (input.isEmpty) {
+      return widget.dataMap;
+    }
+    Map<String, String> result = {};
+    widget.dataMap.forEach((key, value) {
+      if (key.contains(input)) {
+        result[key] = value;
+      }
+    });
+    return result;
   }
 }
